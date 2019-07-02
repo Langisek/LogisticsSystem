f9216598e30b60faac56f83bada127b8f8585b06
David Langer <none@none>
2019-07-02 17:11:43 +0200

    Přidány service třídy

---
+++ src/main/java/com/langi/logisticssystem/business/service/implementation/ProduktServiceImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.langi.logisticssystem.business.service.implementation;
+
+/**
+ *
+ * @author david.langer
+ */
+public class ProduktServiceImpl {
+    
+}
